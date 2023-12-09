d2c7aae8896af93ec53d53619765b46b8b3cc0da
SanketSonawane11 <sanketsonawane424@gmail.com>
2023-12-09 19:21:53 +0530

    Second Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,51 @@ import React, { useState } from 'react'
 import '../Styles/header.css'
 import Link from 'next/link'
 import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { IoBedOutline } from "react-icons/io5";
+import { TbPlaneInflight } from "react-icons/tb";
+import { SiYourtraveldottv } from "react-icons/si";
+import { IoCarSportOutline } from "react-icons/io5";
+import { GiIndianPalace } from "react-icons/gi";
+import { RiTaxiLine } from "react-icons/ri";
+
 
 function Header() {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const subMenuItems = [
+    {
+      title: 'Stays',
+      link: '/',
+      icon: <IoBedOutline />,
+    },
+    {
+      title: 'Flights',
+      link: '/',
+      icon: <TbPlaneInflight />,
+    },
+    {
+      title: 'Flight + Hotel',
+      link: '/',
+      icon: <SiYourtraveldottv />,
+    },
+    {
+      title: 'Car Rentals',
+      link: '/',
+      icon: <IoCarSportOutline />,
+    },
+    {
+      title: 'Attractions',
+      link: '/',
+      icon: <GiIndianPalace />,
+    },
+    {
+      title: 'Airport taxis',
+      link: '/',
+      icon: <RiTaxiLine />,
+    }
+  ];
 
     
   return (
@@ -16,22 +56,47 @@ function Header() {
         
       <nav className='navBar'>
 
-        <Link href="/" className="logo">
-            <h1>Booking.com</h1>
-        </Link>
+        <div className="firstNav">
 
-        <div className="menuOptions flex lg:hidden">
+          <Link href="/" className="logo">
+              Booking.com
+          </Link>
 
-          <button className='burgerButton' onClick={()=>{setMenuOpen(true)}}>
+          <div className="menuOptions flex lg:hidden">
 
-            <RxHamburgerMenu />
-            <span className="sr-only">Open Main Menu</span>
 
-          </button>
+            <div className="profile Button">
+
+              <MdOutlineAccountCircle/>
+
+            </div>
+
+            <button className='burger Button' onClick={()=>{setMenuOpen(true)}}>
+
+              <RxHamburgerMenu />
+              <span className="sr-only">Open Main Menu</span>
+
+            </button>
+
+          </div>
+
+        </div>
+
+        <div className='subNav'>
+
+        {subMenuItems.map((item, index)=>
+          (
+            <button className='navButton Button'>
+              <Link href={item.link}> {item.icon} <p className='btnText'>{item.title}</p> </Link>
+            </button>
+          )
+        )}
 
         </div>
 
       </nav>
+
+      
 
     </header>
   )
